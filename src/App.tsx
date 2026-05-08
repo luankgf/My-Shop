b9124d3f1a89effac1260e54d1edbbd155581474
Luan Farias <luan.klebergf@hotmail.com>
2026-05-07 21:23:22 -0300

    Instala Redux e atualiza dependências

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Header } from "./components/Header/Header";
 import { GlobalStyle } from './styles/GlobalStyles';
 import { ProductsList } from "./components/ProductsList/ProductsList";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
 
       <ProductsList />
 
       <GlobalStyle/>
-    </>
+    </Provider>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,44 @@ import { FiLogIn, FiLogOut, FiShoppingCart} from "react-icons/fi";
 import * as S from "./styles";
 import { Cart } from "../Cart/Cart";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducer } from "../../redux/root-reducer";
 
 export const Header: React.FC = () => {
+    const { user } = useSelector(
+        (rootReducer: RootReducer) => rootReducer.userReducer
+    );
+    const dispatch = useDispatch()
+    
     const [showCart, setShowCart] = useState(false)
-    const isLogged = false
+    const isLogged = user !== null;
 
+    function handleUserAuth () {
+        // usuário não está logado
+        if (user === null) {
+            // despachar a action de login
+            dispatch({
+                type: "user/login",
+                payload: {
+                    name: "Luan Farias",
+                    email: "luan@email.com",
+                },
+            });
+        } else {
+            dispatch({
+                type: "user/logout",
+            });
+        }
+    }
+
+    
     return (
         <S.StyledHeader>
             <S.Wrapper>
                 <S.HeaderTitle>MyShop.</S.HeaderTitle>
 
                 <S.ButtonsWrapper>
-                    <S.AuthButton isLogged={isLogged}>
+                    <S.AuthButton isLogged={isLogged} onClick={handleUserAuth}>
                         { isLogged ? "Logout" : "Login" }
                         { isLogged ? <FiLogOut /> : <FiLogIn /> }
                     </S.AuthButton>
